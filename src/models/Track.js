e6005e94fbd89b581981eef6ba542c4bc49426e7
BatsuraBohdana <batsura.bohdana@chnu.edu.ua>
2026-05-05 20:41:12 +0300

    Update Track.js

---
+++ src/models/Track.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const trackSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'Назва треку є обов’язковою'],
+    trim: true
+  },
+  artist: {
+    type: String,
+    required: [true, 'Автор є обов’язковим'],
+    trim: true
+  },
+  album: {
+    type: String,
+    trim: true
+  },
+  duration: {
+    type: Number, // в секундах
+    min: 0
+  },
+  tags: {
+    type: [String],
+    default: []
+  },
+  url: {
+    type: String,
+    trim: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+
+trackSchema.index({ tags: 1 });
+trackSchema.index({ createdAt: 1 });
+
+module.exports = mongoose.model('Track', trackSchema);
